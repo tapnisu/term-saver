1cfcb21c0a59943be5a0182fc234b52bc3e688cd
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-09 19:59:24 +0700

    Just place text in bottom right corner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor,
+    style::{self},
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use std::io::{self, Write};
+
+const TEXT: &str = "Blazingly fast";
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    let text_length = TEXT.len() as u16;
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let (width, height) = crossterm::terminal::size()?;
+
+    stdout
+        .queue(cursor::MoveTo(width - text_length, height))?
+        .queue(style::Print(TEXT))?;
+
+    stdout.flush()?;
+    Ok(())
 }
