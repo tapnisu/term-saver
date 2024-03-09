a51ebc385394dd14259e3c10bc159560c7fcf4ff
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-09 21:00:07 +0700

    Properly exit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use crossterm::{cursor, style, terminal, ExecutableCommand, QueueableCommand};
 use std::{
     io::{self, Write},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     thread::sleep,
     time::Duration,
 };
 
 const TEXT: &str = "Blazingly fast";
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut stdout = io::stdout();
 
     let text_length = TEXT.chars().count() as u16;
@@ -17,12 +21,26 @@ fn main() -> io::Result<()> {
     let mut x = 20;
     let mut y = 20;
 
-    stdout.execute(terminal::EnterAlternateScreen)?;
+    stdout
+        .execute(cursor::Hide)
+        .unwrap()
+        .execute(terminal::EnterAlternateScreen)
+        .unwrap();
 
-    loop {
-        stdout.execute(terminal::Clear(terminal::ClearType::CurrentLine))?;
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
 
-        let (width, height) = crossterm::terminal::size()?;
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    while running.load(Ordering::SeqCst) {
+        stdout
+            .execute(terminal::Clear(terminal::ClearType::CurrentLine))
+            .unwrap();
+
+        let (width, height) = crossterm::terminal::size().unwrap();
 
         if moving_right {
             x += 1;
@@ -45,10 +63,19 @@ fn main() -> io::Result<()> {
         }
 
         stdout
-            .queue(cursor::MoveTo(x, y))?
-            .queue(style::Print(TEXT))?
-            .flush()?;
+            .queue(cursor::MoveTo(x, y))
+            .unwrap()
+            .queue(style::Print(TEXT))
+            .unwrap()
+            .flush()
+            .unwrap();
 
         sleep(Duration::new(0, 25000000));
     }
+
+    stdout
+        .execute(cursor::Show)
+        .unwrap()
+        .execute(terminal::LeaveAlternateScreen)
+        .unwrap();
 }
